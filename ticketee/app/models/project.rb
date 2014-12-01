5556b6ab7442552d645ed729f27c4b832bb74254
Quan Ta <qta@federatedmedia.net>
2014-11-30 21:51:03 -0800

    Cull tickets when project get destroyed
    How would you setup rspec test for deleting ticket?
    The book just has no new test for deleting ticket.

--- ticketee/app/models/project.rb
+++ ticketee/app/models/project.rb
@@ -1,5 +1,18 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
 
-  has_many :tickets
+  ## Use this if you have callback on Ticket and you have a large
+  ## number of tickets on the project being deleted.
+  # has_many :tickets, dependent: :destroy
+  #
+  ## Use this if you have a large number of tickets and don't have
+  ## callbacks, or have callback that you don't necessarily care about
+  ## when deleting a project
+  # has_many :tickets, dependent: :destroy_all
+  has_many :tickets, dependent: :delete_all
+  #
+  ## Finally, if you just want to disassociate tickets from a project
+  ## and unset the project_id
+  # has_many :tickets, dependent: :nullify
+
 end
